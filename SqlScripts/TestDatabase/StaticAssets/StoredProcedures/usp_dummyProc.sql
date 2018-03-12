634fde2d248df8046d4e4b21c44043adce74ef22
Paul Glavich <glav@theglavs.com>
2018-03-13 09:21:24 +1100

    Adding a proc to other db

---
+++ SqlScripts/TestDatabase/StaticAssets/StoredProcedures/usp_dummyProc.sql
@@ -0,0 +1,30 @@
+﻿/****** Object:  StoredProcedure [dbo].[usp_someProc]    Script Date: 12/03/2018 6:11:46 PM ******/
+DROP PROCEDURE IF EXISTS [dbo].[usp_dummyProc]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_someProc]    Script Date: 12/03/2018 6:11:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usp_dummyProc]') AND type IN (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[usp_dummyProc] AS' 
+END
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[usp_dummyProc] 
+AS
+BEGIN
+	SELECT * FROM [test].[TableOne]
+END
+GO
+
+

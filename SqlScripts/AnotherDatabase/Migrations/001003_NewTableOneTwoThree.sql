b1cadd7c1a999554958b55eb064b9d2908410e84
Paul Glavich <glav@theglavs.com>
2018-03-13 10:47:11 +1100

    New table in anotherdatabase

---
+++ SqlScripts/AnotherDatabase/Migrations/001003_NewTableOneTwoThree.sql
@@ -0,0 +1,14 @@
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[stuff].[OneTwoThree]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [stuff].[OneTwoThree](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SomeNumber] [INT] NULL,
+	[SomeText] [varchar](50) NULL,
+ CONSTRAINT [PK_OneTwoThree] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+END
+GO

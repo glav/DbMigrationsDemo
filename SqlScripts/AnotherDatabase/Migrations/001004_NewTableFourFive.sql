fd283818a062027794bf989d6db1dac222a4858e
Paul Glavich <glav@theglavs.com>
2018-03-13 11:58:15 +1100

    New table FourFive

---
+++ SqlScripts/AnotherDatabase/Migrations/001004_NewTableFourFive.sql
@@ -0,0 +1,15 @@
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[stuff].[FourFive]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [stuff].[FourFive](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[SomeNumber] [INT] NULL,
+	[SomeText] [varchar](50) NULL,
+ CONSTRAINT [PK_FourFive] PRIMARY KEY CLUSTERED 
+(
+	[Id] DESC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+END
+GO
+

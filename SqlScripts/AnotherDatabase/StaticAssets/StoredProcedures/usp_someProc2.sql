01f8d1038f42a559ceae75f227a11b0bb0618baf
Paul Glavich <glav@theglavs.com>
2018-03-15 12:44:57 +1100

    Added someproc2

---
+++ SqlScripts/AnotherDatabase/StaticAssets/StoredProcedures/usp_someProc2.sql
@@ -0,0 +1,30 @@
+﻿/****** Object:  StoredProcedure [dbo].[usp_someProc]    Script Date: 12/03/2018 6:11:46 PM ******/
+DROP PROCEDURE IF EXISTS [dbo].[usp_someProc2]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_someProc]    Script Date: 12/03/2018 6:11:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usp_someProc2]') AND type IN (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[usp_someProc2] AS' 
+END
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[usp_someProc2] 
+AS
+BEGIN
+	SELECT 'hello from someProc2'
+END
+GO
+
+

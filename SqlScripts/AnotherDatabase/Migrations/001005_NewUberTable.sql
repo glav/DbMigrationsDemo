936c18963e519391d4c21a0de3a29de6a128fb2d
Paul Glavich <glav@theglavs.com>
2018-05-31 11:39:15 +1000

    Adding new table

---
+++ SqlScripts/AnotherDatabase/Migrations/001005_NewUberTable.sql
@@ -0,0 +1,15 @@
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[stuff].[FourFive]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [stuff].[Uber](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UberNumber] [INT] NULL,
+	[UberText] [varchar](50) NULL,
+ CONSTRAINT [PK_FourFive] PRIMARY KEY CLUSTERED 
+(
+	[Id] DESC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+END
+GO
+
